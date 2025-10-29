65aea74a37a999b7a0316bf9fd261f4ea26201b4
jcastro-zq <jcastro@zeroq.cl>
2025-10-29 13:19:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 import re
 
 app = Flask(__name__)
 
+API_KEY = "mi_clave_ultra_secreta_123"
+
 @app.route('/divisas', methods=['GET'])
 def get_divisas():
+    key = request.args.get("key")
+    if key != API_KEY:
+        return jsonify({"error": "Unauthorized"}), 401
+
     url = "https://si3.bcentral.cl/indicadoressiete/secure/indicadoresdiarios.aspx"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -15,16 +21,4 @@ def get_divisas():
     def extraer_valor(label):
         match = re.search(rf"{label}[^0-9]+([\d\.,]+)", html_text)
         if match:
-            return float(match.group(1).replace('.', '').replace(',', '.'))
-        return None
-
-    uf = extraer_valor("Unidad de fomento")
-    usd = extraer_valor("Dólar observado")
-
-    return jsonify({
-        "uf": uf,
-        "usd": usd
-    })
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+            return float(match.group(1).replace('.', '').replace(',', '.')
